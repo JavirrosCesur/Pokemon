1fc851973cef058b03ad185b6390f7050b43d35f
Javier <javirros123@gmail.com>
2022-04-29 14:13:26 +0200

    Pausa para crear combate

--- src/entrenador/Entrenador.java
+++ src/entrenador/Entrenador.java
@@ -1,6 +1,10 @@
 package entrenador;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import combate.Combate;
 import pokemon.Pokemon;
 
 public class Entrenador {
@@ -48,4 +52,67 @@ public class Entrenador {
     public void setPokedollar(int pokedollar) {
         this.pokedollar = pokedollar;
     }
+
+    public boolean dejarPokemon(int numero) {
+        if (this.equipo.size() < 2) {
+            System.out.println("No puedes tener menos de un pokemon en el equipo.");
+            return false;
+        }
+
+        else {
+            System.out.println("Has dejado a " + this.equipo.get(numero).getMote() + " en la caja.");
+            this.caja.add(this.equipo.get(numero));
+            this.equipo.remove(numero);
+            return true;
+        }
+    }
+
+    public boolean sacarPokemon(int numero) {
+        if (equipo.size() == 4) {
+            System.out.println("No puedes llevar mas pokemons");
+            return false;
+        } else if (equipo.size() <= 3) {
+            this.equipo.add(this.caja.get(numero));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean capturarPokemon(Pokemon pokemon) {
+        int ratio = (int) (Math.random() * 3);
+        if (ratio == 0) {
+            System.out.println("El pokemon se ha escapado.");
+            return false;
+        } else {
+            System.out.println("Has capturado al pokemon");
+            if (this.equipo.size() == 4) {
+                this.caja.add(pokemon);
+                System.out.println(pokemon.getNombre() + " ha sido añadido a la caja.");
+            } else {
+                this.equipo.add(pokemon);
+                System.out.println(pokemon.getNombre() + " ha sido añadido a tu equipo.");
+            }
+            System.out.println("Ponle un mote a tu pokemon: ");
+            this.nombrarPokemon(pokemon, nombre);
+            return true;
+        }
+    }
+
+    public void nombrarPokemon(Pokemon pokemon, String nombre) {
+        pokemon.setMote(nombre);
+    }
+
+    public boolean ordenPokemon(Pokemon pokemon) {
+        return false;
+    }
+
+    public Boolean cambiarPosiciones(int numero) {
+        Collections.swap(this.equipo, 0, numero);
+        return true;
+    }
+
+    public boolean huirCombate(Entrenador ganador) {
+        return false;
+    }
+
 }
