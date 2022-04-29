67e51b67bf150067ea36089284e18a9741010151
JayCeeKoala <josecarlos.moya.m@gmail.com>
2022-04-29 10:39:17 +0200

    Creacion de los enumerados

---
+++ src/enumerados/EstadoSecundario.java
@@ -0,0 +1,7 @@
+package Pokemon.src.enumerados;
+
+public enum EstadoSecundario {
+    CONFUSO,
+    ENAMORADO,
+    NINGUNO
+}
---
+++ src/enumerados/Tipo.java
@@ -0,0 +1,12 @@
+package Pokemon.src.enumerados;
+
+public enum Tipo {
+    AGUA,
+    FUEGO,
+    PLANTA,
+    ELECTRICO,
+    VOLADOR,
+    ROCA,
+    NORMAL,
+    NINGUNO
+}
