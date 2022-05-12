65e26d4710970ed56c432b3d7311417f16354ab8
JayCeeKoala <josecarlos.moya.m@gmail.com>
2022-05-13 01:30:18 +0200

    Muero

--- src/modelo/combate/Combate.java
+++ src/modelo/combate/Combate.java
@@ -1,31 +1,36 @@
 package Pokemon.src.modelo.combate;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-
 import Pokemon.src.modelo.entrenador.Entrenador;
 
 public class Combate {
 
-    private List<Turno> turnos;
+    private ArrayList<Turno> turnos;
     private Entrenador jugador;
     private Entrenador rival;
     private int koJugador;
     private int koRival;
     private Entrenador ganador;
     private Entrenador perdedor;
+    private static int contadorCombates = 0;
+    public static final String PATH = "Pokemon/log/combate" + contadorCombates + ".log";
 
-    Combate(Entrenador jugador, Entrenador rival, Entrenador ganador, Entrenador perdedor) {
+    Combate(Entrenador jugador, Entrenador rival) {
+        this.addContadorCombates();
         this.turnos = new ArrayList<>();
         this.jugador = jugador;
         this.rival = rival;
         this.koJugador = 0;
         this.koRival = 0;
-        this.ganador = ganador;
-        this.perdedor = perdedor;
+        this.ganador = null;
+        this.perdedor = null;
     }
 
-    public List<Turno> getTurnos() {
+    public ArrayList<Turno> getTurnos() {
         return turnos;
     }
 
@@ -53,7 +58,11 @@ public class Combate {
         return perdedor;
     }
 
-    public void setTurnos(List<Turno> turnos) {
+    public static int getContadorCombates() {
+        return contadorCombates;
+    }
+
+    public void setTurnos(ArrayList<Turno> turnos) {
         this.turnos = turnos;
     }
 
@@ -81,6 +90,14 @@ public class Combate {
         this.perdedor = perdedor;
     }
 
+    public void addContadorCombates(){
+        contadorCombates++;
+    }
+
+    public void addTurno(){
+        this.turnos.add(new Turno("Acción Jugador", "Acción Rival"));
+    }
+
     public void koPokemon(){
         if(this.jugador.getPrimerPokemon().getVitalidadActual() <= 0){
             this.koRival++;
@@ -95,29 +112,40 @@ public class Combate {
             this.setGanador(this.getJugador());
             this.setPerdedor(this.getRival());
             System.out.println("¡Has ganado el combate!");
-            this.getGanador()
-                    .setPokedollar(this.getGanador().getPokedollar() + (this.getPerdedor().getPokedollar() / 3) * 2);
+            this.getGanador().setPokedollar(this.getGanador().getPokedollar() + (this.getPerdedor().getPokedollar() / 3) * 2);
 
-            System.out.println("Has ganado " + (this.getPerdedor().getPokedollar() / 3) + "$ pokedollares.");
         } else if (this.getKoRival() == 4) {
             this.setGanador(this.getRival());
             this.setPerdedor(this.getGanador());
             System.out.println("¡" + this.getRival().getNombre() + " ha ganado el combate!");
             this.getPerdedor().setPokedollar(this.getPerdedor().getPokedollar() / 3);
-            System.out.println("Has perdido " + (this.getPerdedor().getPokedollar() / 3) + "$ pokedollares.");
         }
 
     }
 
-    public boolean huir() {
+    public void huir() {
         this.setGanador(this.getRival());
         this.setPerdedor(this.getJugador());
-
         System.out.println("Te retiras. ¡" + this.getRival().getNombre() + " ha ganado el combate!");
         this.getPerdedor().setPokedollar(this.getPerdedor().getPokedollar() / 3);
-        System.out.println("Has perdido " + (this.getPerdedor().getPokedollar() / 3) + "$ pokedollares.");
+    }
 
-        return false;
+    public void logCombate(){
+        File fichero = new File(PATH);
+        try {
+            FileWriter fw = new FileWriter(fichero);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            for (Turno turno : turnos) {
+                bw.write("Turno " + turno.getNumeroTurno() + ":\n" +
+                "Jugador: " + turno.getAccionJugador() + ".\n" + 
+                "Rival: " + turno.getAccionRival() + ".\n\n");
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
 
 }
